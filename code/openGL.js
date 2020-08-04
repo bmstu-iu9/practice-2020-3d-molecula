160f46fbc8d3f3c1d5552e9d931af0fa3fe37f9f
NotThatWay <haosfortum.random@gmail.com>
2020-08-04 20:16:46 +0300

    Decent parser added

--- code/openGL.js
+++ code/openGL.js
@@ -63,7 +63,7 @@ const getMolecula = () => {
 	if (text) {
 		current_molecula = text;
     		//создание ссылки
-    		let link = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/fastformula/" + current_molecula + "/JSON";
+    		let link = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/fastformula/" + current_molecula + "/JSON?record_type=3d";
 
     		getJSON(link, function(error, data) {
 			if (error != null) {
@@ -71,6 +71,7 @@ const getMolecula = () => {
 			} else {
 				jsonData = data;
 				console.log(data);
+				parseMolecula(jsonData);
 			}
 		});
 	}
