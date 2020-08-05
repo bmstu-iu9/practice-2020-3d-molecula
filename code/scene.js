eba0f0a8fe5adfdd8db2fe99be6f40daaee45dc5
Ga1ath <52465270+Ga1ath@users.noreply.github.com>
2020-08-05 19:13:01 +0300

    Update scene.js

--- code/scene.js
+++ code/scene.js
@@ -16,8 +16,8 @@ const initThreeJS = () => {
 	window.onresize = resizeScene;
 	// создание сцены, ее настройка
 	scene = new THREE.Scene();
-	width = //canvas.width;
-	height = //canvas.height;
+	width = window.innerWidth * 0.8;
+	height = window.innerHeight * 0.7;
 	camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 	renderer = new THREE.WebGLRenderer({canvas: glcanvas});
 	renderer.setSize(width, height);
